d80f349f93e19c56e30ec4b80ddb7dc9b9d47bcc
zhousenshan <275384420@qq.com>
2016-09-10 22:03:10 +0800

    [chg]优化测试文件，更正测试问题

--- src/main/java/com/zeusjava/client/UserClient.java
+++ src/main/java/com/zeusjava/client/UserClient.java
@@ -1,19 +1,22 @@
 package com.zeusjava.client;
 import javax.ws.rs.core.MediaType;
 import javax.xml.bind.JAXBElement;
+
+import org.testng.annotations.Test;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.representation.Form;
 import com.zeusjava.entity.User;
-import org.junit.Test;
 
+@Test
 public class UserClient {
 	private WebResource r = null;
-	@Test
+
 	public void insertUser(){
-		r = Client.create().resource("http://localhost:8081/jersey/api/users");
+		r = Client.create().resource("http://localhost:8080/JerseyRestFul/api/users");
 		Form form = new Form();
 		form.add("userId", "002");
 		form.add("userName", "ZhaoHongXuan");
@@ -22,24 +25,24 @@ public class UserClient {
 				.post(ClientResponse.class, form);
 		System.out.println(response.getStatus());
 	}
-	@Test
+
 	public void findUser(){
-		r = Client.create().resource("http://localhost:8081/jersey/api/users/002");
+		r = Client.create().resource("http://localhost:8080/JerseyRestFul/api/users/002");
 		String jsonRes = r.accept(MediaType.APPLICATION_XML).get(String.class);
 		System.out.println(jsonRes);
 	}
 
-	@Test
+
 	public void updateUser(){
-		r = Client.create().resource("http://localhost:8081/jersey/api/users");
+		r = Client.create().resource("http://localhost:8080/JerseyRestFul/api/users");
 		User user = new User("002","ZhaoXiaoXuan",24);
 		ClientResponse response = r.path(user.getUserId()).accept(MediaType.APPLICATION_XML)
 				.put(ClientResponse.class, user);
 		System.out.println(response.getStatus());
 	}
-	@Test
+
 	public void deleteUser(){
-		r = Client.create().resource("http://localhost:8081/jersey/api/users");
+		r = Client.create().resource("http://localhost:8080/JerseyRestFul/api/users");
 
 		GenericType<JAXBElement<User>> generic = new GenericType<JAXBElement<User>>() {};
 		JAXBElement<User> jaxbContact = r
